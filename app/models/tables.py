4ff9d22605dfdf9634891dec0bd23cf483ab9c24
earthbreaker <alphonceodongo005@gmail.com>
2026-06-16 17:14:21 +0300

    added tables

--- app/models/tables.py
+++ app/models/tables.py
@@ -1,28 +1,52 @@
-credentials= """
-CREATE TABLE IF NOT EXISTS auth_credentials(
-credential_id UUID DEFAULT gen_random_uuid() PRIMARY KEY,
-email TEXT NOT NULL, 
-password TEXT NOT NULL,
-is_verified BOOLEAN false,
-is_revoked BOOLEAN DEFAULT false
-);
-"""
+blog_api_table_preliminary = (
+    """
+    CREATE TABLE IF NOT EXISTS profile(
+    profile_id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    avatar_link TEXT, 
+    social_link TEXT, 
+    user_id UUID
+    FOREIGN KEY user_id REFERENCES users(user_id)
+    )
+    """,
 
-role_type = "CREATE TYPE rolesType AS ENUM ('user', 'admin')"
+    """
+    --credentials
+    CREATE TABLE IF NOT EXISTS oauth2_credential(
+    credential_id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    email TEXT NOT NULL UNIQUE,
+    hashed_password TEXT NOT NULL UNIQUE,
+    is_verified BOOLEAN DEFAULT 'false',
+    created_at TIMESTAMPTZ
+    )
+    """,
+    """
+    CREATE TABLE IF NOT EXISTS roles(
+    role_id INTEGER GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    role_name VARCHAR(30)
+    )
+    """,
+    """
+    CREATE TABLE IF NOT EXISTS preferences(
+    preference_id INTEGER GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    name VARCHAR(50),
+    category VARCHAR(50)
+    )
+    """,
+    """
+    CREATE TABLE IF NOT EXISTS users_preferences(
+    user_id UUID NOT NULL,
+    preference_id NOT NULL,
+    FOREING KEY user_id REFERENCES users(user_id) ON DELETE CASCADE ON UPDATE CASCADE,
+    FOREIGN KEY preference_id REFERENCES preferences(preference_id) ON DELETE CASCADE
+    )
+    """,
+    """
+    CREATE TABLE IF NOT EXISTS users(
+    user_id UUID PRIMARY KEY gen_random uuid(),
+    username VARCHAR(80),
+    role_id INTEGER NOT NULL,
+    credential_id UUID NOT NULL UNIQUE,
+    created_at TIMSTAMPTZ DEFAULT NOW())
 
-users="""
-CREATE TABLE IF NOT EXISTS users(
-user_id UUID DEFAULT gen_random_uuid() PRIMARY KEY,
-user_name VARCHAR(100) UNIQUE NOT NULL,
-role_id VARCHAR(20) DEFAULT 'user',
-credential_id UUID NOT NULL,
-FOREIGN KEY credenatial_id REFERENCES auth_credential(credential_id)
-)"""
-
-tokens = """
-CREATE TABLE IF NOT EXISTS tokens(
-hashed_token TEXT,
-is_used BOOLEAN DEFAULT false,
-expiry TIMESTAMPTZ,
-credential_id UUID
-)"""
+    """
+)
