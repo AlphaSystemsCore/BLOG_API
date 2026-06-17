81a3a062b688ed6ac553c19afa6e91d55d4cc382
earthbreaker <alphonceodongo005@gmail.com>
2026-06-17 14:49:26 +0300

    added requirement.txt

--- app/core/db_config.py
+++ app/core/db_config.py
@@ -1,18 +1,19 @@
 from configparser import ConfigParser
 
-def load_config(filename="database.ini", section="postgres"):
+def load_config(filename="database.ini", section="postgresql"):
     parser = ConfigParser()
     parser.read(filename)
     config ={}
 
     if parser.has_section(section):
-        param = parser.items(section)
+        params = parser.items(section)
         for param in params:
             config[param[0]] = param[1]
-        return param
+        return config
     else:
         raise Exception(f"The section {section} does not exist in the file {filename}")
 
+# for quick test
 if __name__ == "__main__":
     config = load_config()
     print(config)
---
+++ app/db/db_connection.py
@@ -0,0 +1,3 @@
+
+
+from app.core.db_config import load_config
