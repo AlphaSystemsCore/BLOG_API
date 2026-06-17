800ddc6db20c3514d0c8572b137bc7834cfebd03
earthbreaker <alphonceodongo005@gmail.com>
2026-06-17 23:47:16 +0300

    completed signin user

--- app/repositories/auth_repos.py
+++ app/repositories/auth_repos.py
@@ -8,11 +8,31 @@ def get_email(email: str) -> None| tuple:
         email  = cur.fetchone()
     return email
 
-def create_user(emaiL:str, hashed_password: str, hashed_emvt:str):
+def create_user(email:str, hashed_password: str, hashed_emvt:str):
     with get_cur() as cur:
+
         cur.execute(
+            """
             INSERT INTO oauth2_credential
             (hashed_password)
             VALUES(%s) RETURNING credential_id
+            """, (hashed_password,)
+            )
+        credential_id = cur.fetchone()[0]
 
+        cur.execute(
+            """
+            INSERT INTO users
+            (email, credential_id)
+            VALUES(%s,%s) RETURNING user_id
+            """, (email, credential_id)
+        )
+        user_id = cur.fetchone()[0]
+
+        cur.execute(
+            """
+            INSERT INTO email_verification
+            (hashed_token, user_id)
+            VALUES(%s, %s)
+            """, (hashed_emvt, user_id)
         )
--- app/services/auth_service.py
+++ app/services/auth_service.py
@@ -1,5 +1,7 @@
 from app.auth.password_handler import DUMMY_HASH, verify_password, hash_password
-from app.repositories.auth_repos import get_email
+from app.repositories.auth_repos import (
+    get_email,
+    create_user,)
 
 class EmailExistsError(Exception):
     pass
@@ -12,21 +14,25 @@ def gen_email_verification_token():
 
 def signup_user(email:str, password: str):
     # registers_user in the system
-    if len(password) < 8:
-        raise InvalidPasswordLenghtError("Password length must be greater than 7")
-    
-    hashed_password = hash_password(password)
-    if get_email(email):
-        raise EmailExistsError
-    email_verification_token = gen_email_verification_token()
-    hashed_emvt = hash_password(email_verification_token)
     try:
+        if len(password) < 8:
+            raise InvalidPasswordLenghtError("Password length must be greater than 7")
         
+        hashed_password = hash_password(password)
+        if get_email(email):
+            raise EmailExistsError
+        email_verification_token = gen_email_verification_token()
+        hashed_emvt = hash_password(email_verification_token)
+        
+        create_user(email, hashed_password, hashed_emvt)
+    except Exception as e:
+        print("Error: ",e)
+        raise 
     return email_verification_token
     
 
     
 
 if __name__ == "__main__":
-    signup_user("darmaris@cheka.com", "kk")
+    signup_user("darmaris@cheka.com", "kkdadasasdf")
 
