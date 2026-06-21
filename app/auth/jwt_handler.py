10578138a5d46e4bd85a66fb786be437f8bbf6f1
earthbreaker <alphonceodongo005@gmail.com>
2026-06-21 11:44:04 +0300

    added the secrete keys for access and refresh token

--- app/auth/jwt_handler.py
+++ app/auth/jwt_handler.py
@@ -2,6 +2,10 @@ import jwt
 from fastapi.security import OAuth2PasswordBearer
 from datetime import datetime, timedelta, timezone
 
+ALGORITHM = "HS256"
+SECRET_KEY_0 ="43d34b96c25e5cdddcfa4362134f580ee17d2004a2b21004c5dfca79e696bc25"
+SECRET_KEY_1 = "08678f712780dfa5bb45e108ada6f698d2ec565390a3069c2884df5e39e001d9"
+
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="auth/login")
 oauth2_scheme_1 = OAuth2PasswordBearer(tokenUrl="auth/re")
 
