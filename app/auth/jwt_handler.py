c325187ab03baeee3ae76f6e05a50e829d3fc0ea
earthbreaker <alphonceodongo005@gmail.com>
2026-06-21 18:07:39 +0300

    debugged jwt_handler

--- app/auth/jwt_handler.py
+++ app/auth/jwt_handler.py
@@ -3,37 +3,95 @@ from fastapi.security import OAuth2PasswordBearer
 from datetime import datetime, timedelta, timezone
 
 ALGORITHM = "HS256"
-SECRET_KEY_0 ="43d34b96c25e5cdddcfa4362134f580ee17d2004a2b21004c5dfca79e696bc25"
-SECRET_KEY_1 = "08678f712780dfa5bb45e108ada6f698d2ec565390a3069c2884df5e39e001d9"
+ACCESS_TOKEN_SECRET="43d34b96c25e5cdddcfa4362134f580ee17d2004a2b21004c5dfca79e696bc25"
+REFRESH_TOKEN_SECRET = "08678f712780dfa5bb45e108ada6f698d2ec565390a3069c2884df5e39e001d9"
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="auth/login")
-oauth2_scheme_1 = OAuth2PasswordBearer(tokenUrl="auth/re")
 
-def create_token(data: dict, expiry_delta: timedelta|None = None):
-    to_encode = data.copy()
+
+def create_access_token(sub:str,  expiry_delta: timedelta | None = None):
+    to_encode = {
+        "sub": sub,
+        "type":"access_token"
+    }
     if expiry_delta:
         exp = datetime.now(timezone.utc) + expiry_delta
     else: 
         exp = datetime.now(timezone.utc) + timedelta(minutes=15)
     to_encode.update({"exp": exp})
-    encoded = jwt.encode(to_encode, SECRET_KEY_0, algorithm=ALGORITHM)
+    encoded = jwt.encode(to_encode, ACCESS_TOKEN_SECRET, algorithm=ALGORITHM)
     return encoded
 
-def get_current_user(token: Annotated[str, Depends(oauth2_scheme)]):
+def decode_access_token(token: Annotated[str, Depends(oauth2_scheme)]):
     try:
-        payload = jwt.decode(token, SECRET_KEY_1, algorithms=[ALGORITHM])
-        user_id = payload.get("sub")
-    except Exception as e:
-        #
-        raise
+    
+        payload = jwt.decode(token, ACCESS_TOKEN_SECRET, algorithms=[ALGORITHM])
+        if payload.get('type') == "access_token":
+            user_id = payload.get("sub")
+            if user_id == None:
+                raise HTTPException(
+                    status_code=401,
+                    detail="Not authorized"
+                )
+        else: 
+            raise HTTPException(
+                status_code = 401,
+                detail = "Not Authorized"
+            )
+    except jwt.ExpiredSignatureError:
+        raise HTTPException(
+            status_code = 401,
+            detail = "Token expired"
+        )
+    except jwt.InvalidTokenError:
+        raise HTTPException(
+            status_code = 401,
+            detail = "Token not valid"
+        )
+    except HTTPException:
+        raise 
     else: 
         return user_id
 
-def create_refresh_token(data:dict):
-    to_encode = data.copy()
+def create_refresh_token(sub: str, jti: str):
+    to_encode = {
+        "sub": sub,
+        "jti": jti,
+        "type": "refresh_token"
+    }
     exp = datetime.now(timezone.utc) + timedelta(days=30)
     to_encode.update({"exp":exp})
-    encoded = jwt.encode(to_encode, SECRET_KEY_1, algorithm=ALGORITHM)
+    encoded = jwt.encode(to_encode, REFRESH_TOKEN_SECRET, algorithm=ALGORITHM)
     return encoded
 
-def get_refresh_token(refresh_token: str = Depends())
+def decode_refresh_token(token: Annotated[str, Depends(oauth2_scheme)]):
+    try:
+        payload = jwt.decode(token, REFRESH_TOKEN_SECRET, algorithms=[ALGORITHM])
+        if payload.get('type') == 'refresh_token':
+            user_id = payload.get("sub")
+            jti = payload.get("jti")
+            if user_id == None and jti == None:
+                raise HTTPException(
+                    status_code=401,
+                    detail="Not authorized"
+                )
+        else: 
+            raise HTTPException(
+                status_code = 401,
+                detail = "Not Authorized"
+            )
+    except jwt.ExpiredSignatureError:
+        raise HTTPException(
+            status_code = 401,
+            detail = "Invalid token"
+        )
+    except jwt.InvalidTokenError:
+        raise HTTPException(
+            status_code = 401,
+            detail = "Token not valid"
+        )
+    except HTTPException:
+        raise 
+    else:
+        return user_id, jti
+
