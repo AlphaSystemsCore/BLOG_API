520abc82728ff2ef330430bccb25986c7141bca6
earthbreaker <alphonceodongo005@gmail.com>
2026-07-06 20:49:21 +0300

    added PostNotFoundError and FailedToCreatePostError

--- app/services/post_service.py
+++ app/services/post_service.py
@@ -1,30 +1,31 @@
 from app.schemas.post_schemas import PostsIn
 from app.repositories.post_repos import create_post_repo, get_all_post_repo, get_post_by_id_repo, get_post_by_title_repo, delete_post_repo
 
-# Exception have not been handled this is just a basic version of the working code
-# non restricted user actions
+
+class PostNotFoundError(Exception):
+    pass
+class FailedToCreatePostError(Exception):
+    pass
+
 def get_all_post_service():
     """Returns all posts, return a tuple of posts"""
     posts = get_all_post_repo()
     if not posts:
-        # will be change to a defined exception
-        raise ValueError("Post does not exists")
+        return []
     return posts
 
 def get_post_by_id_service(post_id: str):
     """Gets post by post_id"""
     post = get_post_by_id_repo(post_id)
     if post is None:
-        # will be change to a defined exception
-        raise ValueError("Post not found")
+        raise PostNotFoundError("Post not found")
     return post
 
 def get_post_by_title_service(title: str):
     """Gets the post by it's title"""
     post = get_post_by_title_repo(title)
     if post is None:
-        # will be change to a defined exception
-        raise ValueError("Post not found")
+        raise PostNotFoundError("Post not found")
     return post
 
 # user defined actions in posts
@@ -32,8 +33,7 @@ def create_post_service(user_id, post: PostsIn):
     """Creates the post using the PostIn schema in post_schemas"""
     post_id = create_post_repo(user_id, post.title, post.content)
     if post_id is None:
-        # will be change to a defined exception
-        raise ValueError("Post not created")
+        raise FailedToCreatePostError("Failed to create post")
     return post_id[0]
 
 def delete_post_service(user_id, post_id):
