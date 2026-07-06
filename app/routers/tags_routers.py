6e44ec4eedc20744b8f4500bd7d5b482c21d178d
earthbreaker <alphonceodongo005@gmail.com>
2026-07-06 20:05:09 +0300

    added delete_tag endpoint

--- app/routers/tags_routers.py
+++ app/routers/tags_routers.py
@@ -4,13 +4,13 @@ from psycopg2 import errors
 
 from app.auth.jwt_handler import get_current_user
 from app.schemas.tag_schemas import TagsIn
-from app.services.tag_service import get_tags_service, create_tag_service
+from app.services.tag_service import get_tags_service, create_tag_service, delete_tag_service
 from app.exceptions.tag_exceptions import TagNotFoundError
 
 
 tag_router = APIRouter(tags=["tags/hashtags"])
 
-@tag_router.get("/tags/")
+@tag_router.get("/tags")
 def get_tags(user_id: Annotated[str, Depends(get_current_user)]):
     try:
         tags = get_tags_service()
@@ -22,7 +22,7 @@ def get_tags(user_id: Annotated[str, Depends(get_current_user)]):
     else:
         return {"tags": tags}
   
-@tag_router.post("/tags/")
+@tag_router.post("/tags")
 def create_tag(tag: TagsIn, user_id: Annotated[str, Depends(get_current_user)]):
     try:
         create_tag_service(user_id, tag.tag_name, tag.tag_category)
@@ -35,3 +35,13 @@ def create_tag(tag: TagsIn, user_id: Annotated[str, Depends(get_current_user)]):
         return {
             "msg": f"{tag.tag_name}, created successfully."
         }
+
+@tag_router.delete("/tags")
+def delete_tag(tag_id:str, user_id: Annotated[str, get_current_user]):
+    try:
+        delete_tag_service(tag_id)
+    except TagNotFoundError as e:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail=str(e)
+        )
