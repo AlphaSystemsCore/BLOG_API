06eb65773411badcdb70c6cb798bd81025e7e3cd
earthbreaker <alphonceodongo005@gmail.com>
2026-07-29 14:32:06 +0300

    debugged the UUID not define error

--- app/routers/comments_routers.py
+++ app/routers/comments_routers.py
@@ -1,5 +1,6 @@
 from fastapi import APIRouter, status, HTTPException, Depends
 from typing import Annotated
+from uuid import UUID
 
 from app.exceptions.comment_exception import CommentException
 from app.auth.jwt_handler import get_current_user
@@ -23,7 +24,7 @@ UNEXPECTED_ERROR = HTTPException(
         )
 
 @comment_router.post("/comments", response_model=CommentOut)
-def create_comment(comment_in:CommentIn, user_id: Annotated[str, Depends(get_current_user)]):
+def create_comment(comment_in:CommentIn, user_id: Annotated[UUID, Depends(get_current_user)]):
     try:
         return create_comment_service(user_id, comment_in)
     except CommentException as e:
@@ -46,7 +47,7 @@ def get_comments(post_id:str, pagination: Annotated[Pagination, Depends()]):
 
 
 @comment_router.patch("/comment/{comment_id}")
-def update_comment(comment_id:str, user_id:Annotated[str, Depends(get_current_user)]):
+def update_comment(comment_id:str, user_id:Annotated[UUID, Depends(get_current_user)]):
     """update a comment"""
     try:
         return update_comment(comment_id, user_id)
@@ -58,7 +59,7 @@ def update_comment(comment_id:str, user_id:Annotated[str, Depends(get_current_us
 
 
 @comment_router.delete("/comments/{comment_id}", response_model=ResponseComment)
-def delete_comment(comment_id:str, user_id:Annotated[str, Depends(get_current_user)]):
+def delete_comment(comment_id:str, user_id:Annotated[UUID, Depends(get_current_user)]):
     """delete comment/reply """
     try:
         return delete_comment_service(user_id, comment_id)
@@ -92,7 +93,7 @@ def get_replies(current_parent_comment_id):
             detail=str(e)
         )
 
-        
+
 
 
 
