ed198acc8bf73b26c63fe85f376bd54ea0a9307e
earthbreaker <alphonceodongo005@gmail.com>
2026-07-29 15:05:22 +0300

    created TagException that all tags exceptions inherits from

--- app/exceptions/tag_exceptions.py
+++ app/exceptions/tag_exceptions.py
@@ -1,2 +1,10 @@
 class TagNotFoundError(Exception):
     pass
+
+class TagExceptions(Exception):
+   """parent tags exception"""
+   pass
+
+class TagOperationalError(TagExceptions):
+    """any CRUD related error"""
+    pass
