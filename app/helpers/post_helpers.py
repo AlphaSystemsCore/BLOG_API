041b902701f0ced9c662b6d212c3ba2beb868cd6
earthbreaker <alphonceodongo005@gmail.com>
2026-08-07 13:46:28 +0300

    made a dir helper to store helper function for the whole system and kept all post helper in the post_helper.py

---
+++ app/helpers/post_helpers.py
@@ -0,0 +1,56 @@
+def filters_helper(search):
+    """
+    helper, to create, sql filter, by dynamically creating the parameters and condition needed for filtering data
+    """
+    parameters = []
+    conditions = []
+    filters = search.filters.model_dump(exclude_none=True)
+    column_map = {
+        "author":"u.username = %s",
+        "title":"p.title = %s",
+        "content_id": "p.content_id = %s",
+        "status": "p.status = %s",
+        "created_after":"p.created_at >= %s",
+        "created_before":"p.created_at <= %s",
+    }
+
+    for k, v in filters.items():
+        condition = column_map.get(k)
+        if condition is None:
+            raise ValueError(f"Invalid filter field: {k} \nAllowed field fields are {','.join(column_map.keys())}")
+        conditions.append(condition)
+        parameters.append(v)
+    return conditions, parameters
+
+def sort_helper(search):
+    """
+    helper, to extract field and create an order clause for sorting
+    """
+    sort_map = {
+        "title":"p.title",
+        "created_at": "p.created_at",
+        "likes":"likes",
+        "author":"u.username",
+        "comments": "comments",
+        "replies":"replies"
+    }
+    sort_by = search.sort.by
+    sort_column = sort_map.get(sort_by)
+    if sort_column is None:
+        raise ValueError(
+        f"Invalid sort_field {sort_by}\nAllowed field are {', '.join(sort_map.keys())}"
+        )
+    direction = search.sort.direction.upper()
+    order_clause = f" ORDER BY {sort_column} {direction} NULLS LAST "
+    return order_clause
+
+def pagination_helper(search):
+    """
+    helper, extract data from search and create a pagination the return the stringed OFFSET and LIMIT
+    """
+    pagination_clause = []
+    limit = search.pagination.limit
+    offset = search.pagination.offset
+    pagination_clause = f" LIMIT %s OFFSET %s "
+    parameters = [limit, offset]
+    return pagination_clause, parameters
--- app/repositories/post_repos.py
+++ app/repositories/post_repos.py
@@ -1,6 +1,8 @@
 from uuid import UUID
+
 from app.schemas.post_schemas import PostSearch
 from app.db.db_connection import get_cur
+from app.helpers.post_helpers import filters_helper, sort_helper, pagination_helper
 
 def create_post_repo(user_id: UUID, title: str, content: str) ->dict:
     """
@@ -44,58 +46,6 @@ def delete_post_repo(user_id:UUID, content_id: str):
         row = cur.rowcount
     return row
 
-
-def filters_helper(search):
-    "helper, to create, sql filter, by dynamically creating the parameters and condition needed for filtering data"
-    parameters = []
-    conditions = []
-    filters = search.filters.model_dump(exclude_none=True)
-    column_map = {
-        "author":"u.username = %s",
-        "title":"p.title = %s",
-        "content_id": "p.content_id = %s",
-        "status": "p.status = %s",
-        "created_after":"p.created_at >= %s",
-        "created_before":"p.created_at <= %s",
-    }
-
-    for k, v in filters.items():
-        condition = column_map.get(k)
-        if condition is None:
-            raise ValueError(f"Invalid filter field: {k} \nAllowed field fields are {','.join(column_map.keys())}")
-        conditions.append(condition)
-        parameters.append(v)
-    return conditions, parameters
-
-def sort_helper(search):
-    "helper, to extract field and create an order clause for sorting"
-    sort_map = {
-        "title":"p.title",
-        "created_at": "p.created_at",
-        "likes":"likes",
-        "author":"u.username",
-        "comments": "comments",
-        "replies":"replies"
-    }
-    sort_by = search.sort.by
-    sort_column = sort_map.get(sort_by)
-    if sort_column is None:
-        raise ValueError(
-        f"Invalid sort_field {sort_by}\nAllowed field are {', '.join(sort_map.keys())}"
-        )
-    direction = search.sort.direction.upper()
-    order_clause = f" ORDER BY {sort_column} {direction} NULLS LAST "
-    return order_clause
-
-def pagination_helper(search):
-    "helper, extract data from search and create a pagination the return the stringed OFFSET and LIMIT"
-    pagination_clause = []
-    limit = search.pagination.limit
-    offset = search.pagination.offset
-    pagination_clause = f" LIMIT %s OFFSET %s "
-    parameters = [limit, offset]
-    return pagination_clause, parameters
-
 def sql_assembler(search):
     "combines the sql fragments and put them together as an qsl statement, it's the assembler"
     base_query = """
@@ -119,7 +69,6 @@ def sql_assembler(search):
         WHERE 
             u.is_verified = True 
             AND p.is_allowed = True 
-            AND p.status = 'drafted' 
             AND p.deleted_at IS NULL 
             AND cm.deleted_at IS NULL 
         """
